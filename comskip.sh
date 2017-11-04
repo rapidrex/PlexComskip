15f11200d2a666bcddb161ab6f3af01646aa61aa
rapidrex <32749448+rapidrex@users.noreply.github.com>
2017-11-03 23:19:32 -0400

    Update comskip.sh

--- comskip.sh
+++ comskip.sh
@@ -1,26 +1,60 @@
-#!/bin/sh
+#!/bin/bash
 BASEPATH=$(dirname "$1")
 BASEFILE=$(basename "$1")
 FILENAME="${BASEFILE%%.*}"
 FILE_SIZE=$(wc -c < "$1")
 FILE_SIZE=$(echo "${FILE_SIZE}" | awk '{ split( "B KB MB GB TB PB" , v ); s=1; while( $1>1000 ){ $1/=1000; s++ } printf "%.2f %s", $1, v[s] }')
+counter=0
 
-echo "Starting $BASEFILE at $(date) with file size $FILE_SIZE" >> /opt/PlexComskip/"$BASEFILE".txt
+echo "Starting $BASEFILE at $(date) with file size $FILE_SIZE" > /opt/PlexComskip/"$BASEFILE".txt
 
-#/usr/bin/mkvpropedit "$1" --edit track:a1 --set language=eng --edit track:v1 --set language=eng >> /opt/PlexComskip/"$BASEFILE".txt 2>&1
-
-#sleep 10
+while 
+	pgrep HandBrakeCLI > /dev/null && isHBrunning="Running"
+	pgrep HandBrakeCLI > /dev/null || isHBrunning="Not Running"
+	echo "HandBrake is $isHBrunning ... $(date +%r) | counter = $counter" >> /opt/PlexComskip/"$BASEFILE".txt
+	if [[ "$counter" -ne 0 ]]
+	then
+		sleep 2m
+	fi
+	counter=$((counter+1))
+	[[ "$isHBrunning" == "Running" ]]
+do
+    :
+done
 
 /usr/bin/python /opt/PlexComskip/PlexComskip.py "$1" >> /opt/PlexComskip/"$BASEFILE".txt 2>&1
 
-#HandBrakeCLI -Z "Very Fast 720p30" -i "$1" -o "$1".mp4 >> /opt/PlexComskip/"$BASEFILE".txt 2>&1
-HandBrakeCLI --preset-import-file /opt/PlexComskip/bab79.json -Z "BAB79" -i "$1" -o "$1".mp4 >> /opt/PlexComskip/"$BASEFILE".txt 2>&1
-\cp -v -f "$1".mp4 /mnt/6tbTV/downloads/temp/complete/ >> /opt/PlexComskip/"$BASEFILE".txt
+echo "HandBrake encode starting at $(date) " >> /opt/PlexComskip/"$BASEFILE".txt
+echo "-------------------------------------" >> /opt/PlexComskip/"$BASEFILE".txt
+echo "-------------------------------------" >> /opt/PlexComskip/"$BASEFILE".txt
+echo "-------------------------------------" >> /opt/PlexComskip/"$BASEFILE".txt
+
+
+if [[ "$BASEFILE" == *"The Bold and the Beautiful"* ]]
+then
+	HandBrakeCLI --preset-import-file "/opt/PlexComskip/480p30 RF20.json" -Z "480p30 RF20" -i "$1" -o "$1".mp4 >> /opt/PlexComskip/"$BASEFILE".txt 2>&1
+elif [[ "$BASEFILE" == *"The Young and the Restless"* ]]
+then
+	HandBrakeCLI --preset-import-file "/opt/PlexComskip/480p30 RF20.json" -Z "480p30 RF20" -i "$1" -o "$1".mp4 >> /opt/PlexComskip/"$BASEFILE".txt 2>&1
+elif [[ "$BASEFILE" == *"Dateline NBC"* ]]
+then
+	HandBrakeCLI --preset-import-file "/opt/PlexComskip/480p30 RF20.json" -Z "480p30 RF20" -i "$1" -o "$1".mp4 >> /opt/PlexComskip/"$BASEFILE".txt 2>&1
+else
+	HandBrakeCLI --preset-import-file "/opt/PlexComskip/720p30 5.1 RF21.json" -Z "720p30 5.1 RF21" -i "$1" -o "$1".mp4 >> /opt/PlexComskip/"$BASEFILE".txt 2>&1
+fi
+
+echo "HandBrake encode finished at $(date) " >> /opt/PlexComskip/"$BASEFILE".txt
+echo "-------------------------------------" >> /opt/PlexComskip/"$BASEFILE".txt
+echo "-------------------------------------" >> /opt/PlexComskip/"$BASEFILE".txt
+echo "-------------------------------------" >> /opt/PlexComskip/"$BASEFILE".txt
+
+\cp -v -f "$1".mp4 /mnt/6tbTV/downloads/temp/temp/ >> /opt/PlexComskip/"$BASEFILE".txt
 sleep 10
 
 rm -f "$1"
 
+chmod 777 "$1".mp4 
+
 FILE_SIZE=$(wc -c < "$1".mp4)
 FILE_SIZE=$(echo "${FILE_SIZE}" | awk '{ split( "B KB MB GB TB PB" , v ); s=1; while( $1>1000 ){ $1/=1000; s++ } printf "%.2f %s", $1, v[s] }')
 echo "Ended $BASEFILE at $(date) with file size $FILE_SIZE" >> /opt/PlexComskip/"$BASEFILE".txt
-
